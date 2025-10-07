a9c6d2f36bec6376afdb24b2f6ecab136607db43
Aman-Dhakad07 <dhakadaman035@gmail.com>
2025-10-08 03:43:55 +0530

    Update index.js

--- server1/index.js
+++ server1/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:["http://localhost:3000", "http://localhost:3001"] ,  
+        origin:["http://localhost:3000", "http://localhost:3001", "studynotion-liard-beta.vercel.app"] ,  
         credentials:true,
     })
 )
